fc8645ae882a00bdb33aa3f4cb0a28c39e05c49d
Zackkk <zhexie@usc.edu>
2016-01-05 09:21:00 -0800

    bulb updates

--- Math-Bulb Switcher.cs
+++ Math-Bulb Switcher.cs
@@ -1,4 +1,7 @@
-There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
+There are n bulbs that are initially off. You first turn on all the bulbs. 
+Then, you turn off every second bulb. 
+On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). 
+For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
 
 Example:
 
@@ -16,7 +19,8 @@ public class Solution {
     // A bulb ends up on iff it is switched an odd number of times.
     // Bulb i is switched in round d iff d divides i. So bulb i ends up on iff it has an odd number of divisors.
     // Divisors come in pairs, like i=12 has divisors 1 and 12, 2 and 6, and 3 and 4. 
-    // Except if i is a square, like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, and double divisor 6. So bulb i ends up on iff and only if i is a square.
+    // Except if i is a square, like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, 
+    //                          and double divisor 6. So bulb i ends up on iff and only if i is a square.
     // so count the number of square numbers
     public int BulbSwitch(int n) {
         return (int)Math.Sqrt(n); 
